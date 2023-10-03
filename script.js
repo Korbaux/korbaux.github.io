ce944acb843ee3b71cc72db7c2f0760d4c1273d3
Korbo <korbo@ooer.ooo>
2023-10-02 19:49:22 -0500

    merge script with page

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var themeSelect = document.getElementById('theme');
-    themeSelect.onchange = (event) => {
-        less.modifyVars({'@theme': `"${event.target.value}"`})
-    }
- }, false);
